0201fd14f589b2014f47c915625cf636cdb2cacd
Cecile <cecile.leung@oicr.on.ca>
2015-06-08 19:33:38 -0400

    Select only ICGC and aligned specimens; prints donor_unique_id, bam_file_size, specimen type, some accounting

--- donor_download_times+sizes/aligned_bam_size.py
+++ donor_download_times+sizes/aligned_bam_size.py
@@ -5,6 +5,7 @@ import urllib
 import gzip
 import json
 import os
+import fnmatch
 
 url = "http://pancancer.info/gnos_metadata/latest/donor_p_150608020205.jsonl.gz"
 
@@ -16,33 +17,51 @@ def get_raw_jsonl(url):
     unzipped_jsonl = gzip.open("donor_p_150608020205.jsonl.gz", "rb")
     return unzipped_jsonl
 
+def is_tcga(donor):
+    """Check if this donor is from TCGA (US projects)"""
+    return fnmatch.fnmatch(donor['dcc_project_code'], '*-US')
 
-def process_one_sample(json_obj):
+
+def process_one(donor_id, aligned_bam, type):
+    """Get all information of interest from a specimen"""
+    # control/tumour, unique donor id, gnos repo, bam file size, project-ID, number of tumour... # TODO other info?
     #stub
-    print json_obj['aligned_tumor_specimens'][0]['aliquot_id']
-############### this breaks if there is aligned_tumor_specimens is [], i.e. unaligned! ######
+    print '\t'.join([donor_id, str(aligned_bam.get('bam_file_size')), type])
+    return True
+
 
 def main():
     donor_samples_jsonl = get_raw_jsonl(url)
 
     data = donor_samples_jsonl.read()
-    datalines = data.splitlines()
+    list_of_jsons = data.splitlines()
 
+    print "Number of donors before removing TCGA and unaligned: %d" % len(list_of_jsons)
 
-    for i in xrange(len(datalines)):
-        sample = json.loads(datalines[i])  # one line = one json object = one sample
-        process_one_sample(sample)
+    number_of_tcga_donors = 0
+    number_of_aligned_controls = 0
+    number_of_donors_with_aligned_tumour = 0
+    number_of_tumour_specimens = 0
 
+    for i in xrange(len(list_of_jsons)):
+        donor = json.loads(list_of_jsons[i])  # one line = one json object = one donor
+#        import pdb; pdb.set_trace()
+        if is_tcga(donor):
+            number_of_tcga_donors += 1
+            continue  # disregard TCGA donors
+        else:
+            if donor.get('normal_alignment_status') and donor.get('normal_alignment_status').get('aligned_bam'):
+                process_one(donor['donor_unique_id'], donor['normal_alignment_status']['aligned_bam'], 'control')
+                number_of_aligned_controls += 1
 
+            if donor.get('tumor_alignment_status'):
+                for aligned_bam in donor.get('tumor_alignment_status'):
+                    if aligned_bam.get('aligned_bam'):
+                        process_one(donor['donor_unique_id'], aligned_bam.get('aligned_bam'), 'tumour')
+                        number_of_tumour_specimens += 1
 
-    #select only ICGC
-    #select only aligned
-
-    #select control
-    #sum tumour
-    # add columns for # of tumours, unique donor_id, project id, repo, control/tumour
-
-
+    print "Number of aligned controls: %d" % number_of_aligned_controls
+    print "Number of tumour specimens: %d" % number_of_tumour_specimens
 
 if __name__ == '__main__':
     main()
