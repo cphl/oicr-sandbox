86a6b61a4841e269994d4bed6b5ed0dae6f581bc
Cecile <cecile.leung@oicr.on.ca>
2015-06-15 18:27:00 -0400

    Grab zipped cost report if local copy is older than 1 day, unzip it to pwd

--- aws/cost_reporting/cost_reporting.py
+++ aws/cost_reporting/cost_reporting.py
@@ -6,40 +6,35 @@ import time
 
 def getFileFromBucket():
     """Grab today's billing report from the S3 bucket, extract into pwd"""
-    conn = boto.connect_s3()
-    mybucket = conn.get_bucket('oicr.detailed.billing')
 
     prefix = "794321122735-aws-billing-detailed-line-items-with-resources-and-tags-"
     csv_filename = prefix + str(datetime.date.today().isoformat()[0:7]) + ".csv"
     zip_filename = csv_filename + ".zip"
-    import pdb; pdb.set_trace()
 
-    # arrrgh file name only goes up to month granularity
-    # check file modified time
-
-    # See if we have the unzipped .csv file
-    if not os.path.isfile(csv_filename):
-        # See if we have the .zip file
-        if not os.path.isfile(zip_filename):
-            print "Downloading " + zip_filename + "..."
-            mykey = mybucket.get_key(zip_filename)
-            mykey.get_contents_to_filename(zip_filename)
+    # If local data is older than 1 day, download fresh data.
+    mod_time = os.path.getmtime(csv_filename)
+    if datetime.date.today() - datetime.date.fromtimestamp(mod_time) > datetime.timedelta(days=1):
+        conn = boto.connect_s3()
+        mybucket = conn.get_bucket('oicr.detailed.billing')
+        print "Downloading " + zip_filename + "..."
+        mykey = mybucket.get_key(zip_filename)
+        mykey.get_contents_to_filename(zip_filename)
         print "Extracting to file " + csv_filename + "..."
-        zf = zipfile.ZipFile(filename_string)
+        zf = zipfile.ZipFile(zip_filename)
         zf.extractall()
-
 
 
 def getPeople():
     """Return all the different names in the KEEP-tags"""
     people = []
+    import pdb; pdb.set_trace()
     #stub
     return people
 
 
 def main():
     getFileFromBucket()
-    getPeople()
+    list_of_peepz = getPeople()
 
 if __name__ == '__main__':
     main()
