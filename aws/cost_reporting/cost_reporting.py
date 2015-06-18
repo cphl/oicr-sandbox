40cf4dbe041fa70b7216339c153d087b2cd229cf
Cecile <cecile.leung@oicr.on.ca>
2015-06-18 12:39:45 -0400

    Reports sorted by KEEP and PROD tags. Totals for user.

--- aws/cost_reporting/cost_reporting.py
+++ aws/cost_reporting/cost_reporting.py
@@ -4,7 +4,11 @@ import zipfile
 import os.path
 import time
 import csv
+from operator import itemgetter
 
+untagged_full_report = 'untagged.csv'
+untagged_summary_report = 'untagged_operations_totals.csv'
+tagged_full_report = 'tagged.csv'
 
 def getFileFromBucket():
     """Grab today's billing report from the S3 bucket, extract into pwd"""
@@ -27,22 +31,18 @@ def getFileFromBucket():
     return csv_filename
 
 
-def read_file(sourcefile):
-    """do things with the csv file
-        - get all untagged (right now includes empty string tags) KEEP sorted by Operation
-        - get all distinct names from KEEP-tags
-        - ...
-        ...
+def process_untagged(sourcefile):
+    """ Write out TSV files that can be uploaded for others to access:
+            - One has all untagged resources sorted by Operation and totalled.
+            - Other has only a summary of totals with Operation and Cost.
+        Usage that is either untagged or tagged with an empty string (indistinguishable in the cost report)
     """
-
     with open(sourcefile) as csvfile:
         reader = csv.DictReader(csvfile)
 
-        tupled_data = []
+        tupled_data = []  # list of (op, whole row)
         tupled_data_index = set()
 
-        #print dir(reader)
-        #keepers = set([])
         for row in reader:
             tupled_data.append((row['Operation'], row))
             tupled_data_index.add(row['Operation'])
@@ -56,25 +56,27 @@ def read_file(sourcefile):
                 sorted_data.append(match)
 
         operations_totals = []
-        with open('untagged.csv', 'w') as csvfile:
+        print "Generating report for untagged resources: " + untagged_full_report + "..."
+        with open(untagged_full_report, 'w') as csvfile:
             # out put all my data yo
-            fieldnames = ['Operation', 'Cost', 'user:KEEP', 'user:PROD', 'RecordId',
-                          'RateId', 'PricingPlanId', 'UsageType', 'AvailabilityZone',
-                          'ItemDescription', 'UsageQuantity', 'Rate', 'Cost', 'ResourceId']
+            fieldnames = ['Operation', 'Cost', 'user:KEEP', 'user:PROD', 'RecordId', 'RateId', 'PricingPlanId',
+                          'UsageType', 'AvailabilityZone', 'ItemDescription', 'UsageQuantity', 'Rate', 'Cost',
+                          'ResourceId']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             prev_op = sorted_data[0]['Operation']  # initialize
             op_total = 0
             for line in sorted_data:
                 if line['RecordType'] == "LineItem":
-                    if line['Operation'] != prev_op:
-                        writer.writerow({'Operation': prev_op + ' total', 'Cost': op_total})
-                        operations_totals.append({'Operation': prev_op + ' total', 'Cost': op_total})
-                        op_total = 0
-                        # import pdb; pdb.set_trace()
-                        prev_op = line['Operation']
-                    if line['user:KEEP'] == "" and line['user:PROD'] == "":
 
+                    # Select all the untagged, INCLUDING those that are tagged but with an empty string
+                    if line['user:KEEP'] == "" and line['user:PROD'] == "":
+                        if line['Operation'] != prev_op:
+                            writer.writerow({'Operation': prev_op + ' total', 'Cost': op_total})
+                            operations_totals.append({'Operation': '"' + prev_op + '" total', 'Cost': op_total})
+                            op_total = 0
+                            # import pdb; pdb.set_trace()
+                            prev_op = line['Operation']
                         writer.writerow({'Operation': line['Operation'], 'Cost': line['Cost'],
                                          'user:KEEP': line['user:KEEP'], 'user:PROD': line['user:PROD'],
                                          'RecordId': line['RecordId'], 'RateId': line['RateId'],
@@ -83,11 +85,12 @@ def read_file(sourcefile):
                                          'ItemDescription': line['ItemDescription'], 'UsageQuantity': line['UsageQuantity'],
                                          'Rate': line['Rate'], 'Cost': line['Cost'], 'ResourceId': line['ResourceId']})
                         op_total += float(line['Cost'])
-            writer.writerow({'Operation': prev_op + ' total', 'Cost': op_total})            # get that last one!
-            operations_totals.append({'Operation': prev_op + ' total', 'Cost': op_total})   # get that last one!
+            writer.writerow({'Operation': prev_op + ' total', 'Cost': op_total})                    # get that last one!
+            operations_totals.append({'Operation': '"' + prev_op + '" total', 'Cost': op_total})    # get that last one!
 
+        print "Generating summary of totals for untagged resources: " + untagged_summary_report
         total_untagged_cost = 0
-        with open('untagged_operations_totals.csv', 'w') as total_file:
+        with open(untagged_summary_report, 'w') as total_file:
             fields = ['Operation', 'Cost']
             w = csv.DictWriter(total_file, fieldnames=fields)
             w.writeheader()
@@ -96,53 +99,52 @@ def read_file(sourcefile):
                 total_untagged_cost += row['Cost']
             w.writerow({'Operation': 'Overall untagged operations total', 'Cost': total_untagged_cost})
 
-            # if row['user:KEEP'] is not "":
-            # keepers.add(row['user:KEEP'])
 
-    # return keepers
-
-
-def write_untagged_report(sourcefile):
-    """ Write out a TSV file that can be uploaded for others to access.
-        Usage that is untagged by anyone.
-    """
-    with open('untagged.csv', 'w') as csvfile:
-        fieldnames = ['Operation', 'Cost']
-        w = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        w.writeheader()
-        with open(sourcefile) as src:
-            reader = csv.DictReader(src)
-            import pdb; pdb.set_trace()
-            for row in reader:
-                w.writerow({'Operation': row['Operation'], 'ItemDescription': row['ItemDescription'], 'Cost': row['Cost']})
-    #stub
-    return ""
-
-
-def write_empty_tag_report():
-    """ Write out a TSV file that can be uploaded for others to access.
-        Usage that is tagged but with an empty string.
-    """
-    #stub
-    return ""
-
-
-def write_users_report():
+def process_tagged(sourcefile):
     """ Write out a TSV file that can be uploaded for others to access.
         Usage tagged by name in the KEEP-tag.
     """
+    with open(sourcefile) as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        keep_tagged_data = []
+        for row in reader:
+            if row['user:KEEP'] != "":
+                keep_tagged_data.append(row)
+        keep_tagged_data = sorted(keep_tagged_data, key=itemgetter('user:KEEP', 'user:PROD', 'Operation'))
+
+    print "Generating report of tagged resources: " + tagged_full_report
+    with open(tagged_full_report, 'w') as f:
+        fields = ['user:KEEP', 'user:PROD', 'Operation', 'RecordId', 'RateId', 'PricingPlanId',
+                          'UsageType', 'AvailabilityZone', 'ItemDescription', 'UsageQuantity', 'Rate', 'Cost',
+                          'ResourceId']
+        w = csv.DictWriter(f, fieldnames=fields)
+        w.writeheader()
+        keeper_sum = 0
+        keeper = keep_tagged_data[0]['user:KEEP']  # first one might be crap
+        for row in keep_tagged_data:
+            w.writerow({'user:KEEP': row['user:KEEP'], 'user:PROD': row['user:PROD'], 'Operation': row['Operation'],
+                        'RecordId': row['RecordId'], 'RateId': row['RateId'], 'PricingPlanId': row['PricingPlanId'],
+                        'UsageType': row['UsageType'], 'AvailabilityZone': row['AvailabilityZone'],
+                        'ItemDescription': row['ItemDescription'], 'UsageQuantity': row['UsageQuantity'],
+                        'Rate': row['Rate'], 'Cost': row['Cost'], 'ResourceId': row['ResourceId']})
+            # do summing stuff
+            keeper_sum += float(row['Cost'])
+            if keeper != row['user:KEEP']:
+                w.writerow({'Rate': 'Total for user in KEEP tag', 'Cost': keeper_sum})
+                keeper_sum = 0
+                keeper = row['user:KEEP']
+
+
     #stub
     return ""
 
 
 def main():
     csv_filename = getFileFromBucket()
-    read_file(csv_filename)
-    # import pdb; pdb.set_trace()
+    # process_untagged(csv_filename)
 
-    # write_untagged_report(csv_filename)
-    # write_empty_tag_report()
-    # write_users_report()
+    process_tagged(csv_filename)
 
 
 if __name__ == '__main__':
