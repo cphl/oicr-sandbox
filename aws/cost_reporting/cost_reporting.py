46c3cbc11948331db08e4930ddae33582311a974
Cecile <cecile.leung@oicr.on.ca>
2015-06-18 16:44:04 -0400

    Subtotals for production and non-production for each user

--- aws/cost_reporting/cost_reporting.py
+++ aws/cost_reporting/cost_reporting.py
@@ -9,6 +9,7 @@ from operator import itemgetter
 untagged_full_report = 'untagged.csv'
 untagged_summary_report = 'untagged_operations_totals.csv'
 tagged_full_report = 'tagged.csv'
+tagged_summary_report = 'tagged_user_totals.csv'
 
 def getFileFromBucket():
     """Grab today's billing report from the S3 bucket, extract into pwd"""
@@ -70,7 +71,7 @@ def process_untagged(sourcefile):
                 if line['RecordType'] == "LineItem":
 
                     # Select all the untagged, INCLUDING those that are tagged but with an empty string
-                    if line['user:KEEP'] == "" and line['user:PROD'] == "":
+                    if line['user:KEEP'] == "":  # and line['user:PROD'] == "":
                         if line['Operation'] != prev_op:
                             writer.writerow({'Operation': prev_op + ' total', 'Cost': op_total})
                             operations_totals.append({'Operation': '"' + prev_op + '" total', 'Cost': op_total})
@@ -105,8 +106,9 @@ def process_tagged(sourcefile):
         Usage tagged by name in the KEEP-tag.
     """
     with open(sourcefile) as csvfile:
-        reader = csv.DictReader(csvfile)
 
+        # Get data and sort it by KEEP and PROD tags
+        reader = csv.DictReader(csvfile)
         keep_tagged_data = []
         for row in reader:
             if row['user:KEEP'] != "":
@@ -115,35 +117,84 @@ def process_tagged(sourcefile):
 
     print "Generating report of tagged resources: " + tagged_full_report
     with open(tagged_full_report, 'w') as f:
-        fields = ['user:KEEP', 'user:PROD', 'Operation', 'RecordId', 'RateId', 'PricingPlanId',
-                          'UsageType', 'AvailabilityZone', 'ItemDescription', 'UsageQuantity', 'Rate', 'Cost',
-                          'ResourceId']
+        fields = ['user:KEEP', 'user:PROD', 'Operation', 'RecordId', 'RateId', 'PricingPlanId', 'UsageType',
+                  'AvailabilityZone', 'ItemDescription', 'UsageQuantity', 'Rate', 'Cost', 'ResourceId']
         w = csv.DictWriter(f, fieldnames=fields)
         w.writeheader()
+        w.writerow({})
+
+        # accounting
         keeper_sum = 0
-        keeper = keep_tagged_data[0]['user:KEEP']  # first one might be crap
+        keeper_sums = []
+        prev_keeper = keep_tagged_data[0]['user:KEEP']  # first one might be crap
+        non_prod_sum = 0
+        prod_sum = 0
+        non_prod_sums = []
+        prod_sums = []
+
+        # TODO: final total dump out
+
         for row in keep_tagged_data:
+
+            # If we're at a new keeper, dump the info about the last keeper
+            if prev_keeper != row['user:KEEP']:
+                if True:  # non-production subtotal for this keeper
+                    w.writerow({'Rate': 'non-production total', 'Cost': non_prod_sum})
+                    non_prod_sums.append(non_prod_sum)
+                    non_prod_sum = 0
+
+                if True:  # production total for this keeper
+                    w.writerow({'Rate': 'production total', 'Cost': prod_sum})
+                    prod_sums.append(prod_sum)
+                    prod_sum = 0
+
+                w.writerow({'Rate': 'Total for "' + prev_keeper + '" in KEEP tag', 'Cost': keeper_sum})
+                w.writerow({})
+                keeper_sums.append({'user:KEEP': prev_keeper, 'Cost': keeper_sum})
+                keeper_sum = 0
+                prev_keeper = row['user:KEEP']
+
+            # Subtotals for each keeper's production or non-production resources
+            if row['user:PROD'] == "":
+                non_prod_sum += float(row['Cost'])
+            else:
+                prod_sum += float(row['Cost'])
+
+            # Deal with the individual row
             w.writerow({'user:KEEP': row['user:KEEP'], 'user:PROD': row['user:PROD'], 'Operation': row['Operation'],
                         'RecordId': row['RecordId'], 'RateId': row['RateId'], 'PricingPlanId': row['PricingPlanId'],
                         'UsageType': row['UsageType'], 'AvailabilityZone': row['AvailabilityZone'],
                         'ItemDescription': row['ItemDescription'], 'UsageQuantity': row['UsageQuantity'],
                         'Rate': row['Rate'], 'Cost': row['Cost'], 'ResourceId': row['ResourceId']})
+
             # do summing stuff
             keeper_sum += float(row['Cost'])
-            if keeper != row['user:KEEP']:
-                w.writerow({'Rate': 'Total for user in KEEP tag', 'Cost': keeper_sum})
-                keeper_sum = 0
-                keeper = row['user:KEEP']
 
+        # Cheat: final dump for last member, fix later
+        w.writerow({})
+        w.writerow({'Rate': 'non-production total', 'Cost': non_prod_sum})
+        w.writerow({'Rate': 'production total', 'Cost': prod_sum})
+        w.writerow({'Rate': 'Total for "' + prev_keeper + '" in KEEP tag', 'Cost': keeper_sum})
+        non_prod_sums.append(non_prod_sum)
+        prod_sums.append(prod_sum)
+        keeper_sums.append({'user:KEEP': prev_keeper, 'Cost': keeper_sum})
 
-    #stub
-    return ""
+    print "Generating summary of tagged resources, costs by KEEP-tag: " + tagged_summary_report
+    with open(tagged_summary_report, 'w') as f2:
+        fields2 = ['KEEP-tag entry', 'Cost']
+        w2 = csv.DictWriter(f2, fieldnames=fields2)
+        w2.writeheader()
+        # can't think, fix later
+        for i in range(len(keeper_sums)):
+            w2.writerow({})
+            w2.writerow({'KEEP-tag entry': keeper_sums[i]['user:KEEP'] + ' non-production', 'Cost': non_prod_sums[i]})
+            w2.writerow({'KEEP-tag entry': keeper_sums[i]['user:KEEP'] + ' production', 'Cost': prod_sums[i]})
+            w2.writerow({'KEEP-tag entry': keeper_sums[i]['user:KEEP'] + ' total', 'Cost': keeper_sums[i]['Cost']})
 
 
 def main():
     csv_filename = getFileFromBucket()
     # process_untagged(csv_filename)
-
     process_tagged(csv_filename)
 
 
