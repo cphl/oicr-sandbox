cfc2692299bda7721d9681d0b9cc1be9c8b81286
Cecile <cecile.leung@oicr.on.ca>
2015-06-22 17:55:04 -0400

    Correct language for parallelizing. Add column for excluded download time.

--- donor_download_times+sizes/by_whitelist/bwa_runtime_by_whitelist.py
+++ donor_download_times+sizes/by_whitelist/bwa_runtime_by_whitelist.py
@@ -47,12 +47,11 @@ def get_times(lanes_timings):
         Return dict of...lots of stuff
     """
 
-    # If all lanes run parallel for each step, this would help estimate a lower bound, only at specimen level
+    # Assume all lanes run parallel for a particular process. This gives an absolute lower bound (specimen level).
     bwa_max = 0
     download_max = 0
     merge_max = 0
     qc_max = 0
-
     summaxes = 0
 
     # If all lanes run sequentially for each step, this gives the upper bound. (Absolute wrt these metrics)
@@ -60,8 +59,9 @@ def get_times(lanes_timings):
     download_sum = 0
     merge_sum = 0
     qc_sum = 0
-
     sumsums = 0
+
+    no_dl = 0
 
     for lane in lanes_timings:
         if 'metrics' in lane:
@@ -81,13 +81,13 @@ def get_times(lanes_timings):
             merge_sum += merge
             qc_sum += qc
 
-            sumsums += bwa + download + merge + qc
-
-    summaxes += bwa_max + download_max + merge_max + qc_max
+            sumsums += bwa + merge + qc + download
+            no_dl += bwa + merge + qc
+    summaxes += bwa_max + merge_max + qc_max  + download_max
 
     return {'bwa_max': bwa_max, 'bwa_sum': bwa_sum, 'download_max': download_max, 'download_sum': download_sum,
             'merge_max': merge_max, 'merge_sum': merge_sum, 'qc_max': qc_max, 'qc_sum': qc_sum,
-            'sum_of_max_times': summaxes, 'sum_all': sumsums}
+            'sum_of_max_times': summaxes, 'sum_all': sumsums, 'no_dl': no_dl}
 
 
 
@@ -108,8 +108,9 @@ def main():
     #         f.flush()
 
     print '\t'.join(["donor_unique_id", "type", "bwa_max", "bwa_sum", "download_max", "download_sum", "merge_max",
-                     "merge_sum", "qc_max", "qc_sum", "sum_of_max_times", "sum_all"])
+                     "merge_sum", "qc_max", "qc_sum", "sum_of_max_times", "sum_all", "no_dl"])
     total_time = 0
+    total_parallel_time = 0
     number_donors = 0
 
     for d in whitelist_donors:
@@ -121,13 +122,14 @@ def main():
             if control_specimen.get('is_aligned'):
                 lanes_timings = control_specimen.get('alignment').get('timing_metrics')
                 times = get_times(lanes_timings)
-            # import pdb; pdb.set_trace()
+
             print '\t'.join([d.get('donor_unique_id'), specimen_type, str(times['bwa_max']), str(times['bwa_sum']),
                              str(times['download_max']), str(times['download_sum']), str(times['merge_max']),
                              str(times['merge_sum']), str(times['qc_max']), str(times['qc_sum']),
-                             str(times['sum_of_max_times']), str(times['sum_all'])])
+                             str(times['sum_of_max_times']), str(times['sum_all']), str(times['no_dl'])])
+            total_time += times['sum_all']
+            total_parallel_time += times['sum_of_max_times']
 
-        # import pdb; pdb.set_trace()
         # Process the tumor specimen(s) if they exist
         if 'aligned_tumor_specimens' in d:
             specimen_type = "tumour"
@@ -142,15 +144,19 @@ def main():
                 print '\t'.join([d.get('donor_unique_id'), specimen_type, str(times['bwa_max']), str(times['bwa_sum']),
                                  str(times['download_max']), str(times['download_sum']), str(times['merge_max']),
                                  str(times['merge_sum']), str(times['qc_max']), str(times['qc_sum']),
-                                 str(times['sum_of_max_times']), str(times['sum_all'])])
+                                 str(times['sum_of_max_times']), str(times['sum_all']), str(times['no_dl'])])
+                total_time += times['sum_all']
+                total_parallel_time += times['sum_of_max_times']
 
-
-        # total_time += 0
         number_donors += 1
 
     print "\nTotal time:\t%d" % total_time
+    print "Total time with full parallelization within a specimen: \t%d" % total_parallel_time
     print "Total donors included:\t%d" % number_donors
-
+    time_per_donor_full = total_time / number_donors
+    time_per_donor_parallel = total_parallel_time / number_donors
+    print "Time per donor (full):\t%d\tor in days:\t%.1f" % (time_per_donor_full, (time_per_donor_full/3600./24.))
+    print "Time per donor (full parallelization):\t%d\tor in days:\t%.1f" % (time_per_donor_parallel, (time_per_donor_parallel/3600./24.))
 
 if __name__ == '__main__':
     main()
