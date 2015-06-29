4610a39c1905e53113e9af2291d1c3218c2a20bd
Cecile <cecile.leung@oicr.on.ca>
2015-06-29 14:10:17 -0400

    Kinda not working, moving on to different approach This one was: gather keeper's resources, look for resources in untagged and grab them.

--- aws/cost_reporting/cost_reporting_data.py
+++ aws/cost_reporting/cost_reporting_data.py
@@ -16,13 +16,17 @@ class SpreadsheetCache(object):
             for row in tempdata:
                 if float(row['Cost']) != 0 and row['RecordType'] == "LineItem":
                     if row['Operation'] == "" and row['UsageType'] == "":
-                        row['Operation'] = "'ProductName = AWS Support (Developer)'"
-                        row['UsageType'] = "'ProductName = AWS Support (Developer)'"
+                        row['Operation'] = "ProductName" + row['ProductName']
+                        row['UsageType'] = "ProductName" + row['ProductName']
                     self.spreadsheet.append(row)
+        self.fix_case()
+        self.sort_data()
         self.keepers = set()
-        for row in spreadsheet:
-            keepers.add(row['user:KEEP'])
-            # This is wrong, but I'm so tired.
+        for row in self.spreadsheet:
+            self.keepers.add(row['user:KEEP'])
+
+        # initial value for list of untagged items, will have items removed as tagged lists are populated
+        self.unkept = self.spreadsheet
 
     def data(self):
         """Returns spreadsheet (list of dicts)"""
@@ -58,12 +62,57 @@ class SpreadsheetCache(object):
         self.spreadsheet = sorted(self.spreadsheet, key=itemgetter('user:KEEP', 'user:PROD', 'ResourceId',
                                                                    'Operation', 'UsageType', 'Cost'))
 
+    def get_resources_for_one_keeper(self, keeper):
+        """ Includes resources untagged earlier, if they are tagged on the day of billing data download.
+        Alters running list of untagged resource line items.
+        :param keeper: String representing the person associated with the resources
+        :return: List of dicts, each dict is one line item belonging to the keeper
+        """
+        line_items_of_keeper = []
+        resource_ids = set()
+        for row in self.spreadsheet:
+            if row['user:KEEP'] == keeper:
+                resource_ids.add(row['ResourceId'])
+                print "."
+        # Now (go back in time and) grab all the line items with these resource IDs even if untagged in past
+        for row in self.spreadsheet:
+            if row['ResourceId'] in resource_ids:
+                print "-"
+                line_items_of_keeper.append(row)
+                self.unkept.remove(row)
+        return line_items_of_keeper
+
+    # def get_resources_for_keepers(self):
+    #     """ Iterate through list of keepers to populate resource lists for all
+    #     :return: list of list of dicts (i.e. list of {keeper: list of their resources})
+    #     """
+    #     self.keepers_data = []  # list of dicts: keepers with their resources
+    #         for row in self.spreadsheet:
+    #             if row['user:KEEP'] == "":
+    #                 keeper = 'untagged'
+    #             else:
+    #                 keeper = row['user:KEEP']
+    #             # This won't work, I don't think. Also I need this to do something else. Abandon ship.
+    #             self.keepers_data.append(dict(keeper=keeper, list_of_resources=list_of_resources.append{row}))
+
+def get_resources_for_keepers():
+    """populate keepers' line item lists, depopulate unkept list (starts as full)
+    :return: dict of dicts: key = keeper, value = list of dicts, each dict represents a line item
+    """
+    keepers_line_items = {}
+    for keeper in SC.keepers:
+        if keeper != "":
+            print "Getting line items for " + keeper
+            keepers_line_items[keeper] = SC.get_resources_for_one_keeper(keeper)
+    return keepers_line_items
+
 
 def main():
-    SC.fixcase()
-    SC.sort_data()
-    get_filtered_data()
-    generate_reports()
+    keepers_line_items = get_resources_for_keepers()
+    kli0 = keepers_line_items[0]
+    import pdb; pdb.set_trace()
+    # get_filtered_data()
+    # generate_reports()
 
 if __name__ == '__main__':
     SC = SpreadsheetCache()
