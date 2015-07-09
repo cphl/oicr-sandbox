94a4776baa5f83d900d50183b30524e82e19df92
Cecile <cecile.leung@oicr.on.ca>
2015-07-09 17:03:45 -0400

    [aws resource usage] Works but has a few nice-to-haves. See TODOs in code.

--- aws/usage_data.py
+++ aws/usage_data.py
@@ -4,6 +4,10 @@ __author__ = 'cleung'
 # Volumes show associated instances and the KEEP-tags thereof
 # Code borrowed heavily from Niall's previous script: volume_cleanup.py
 
+# TODO: This works, but would be nice to have descriptions/names of associated resources as they are
+# TODO: easier for humans to identify than the resource_ids
+# TODO: Consider making it TSV so that we can list snapshots (or other fields) with commas and it'll still work
+
 import os
 import sys
 import boto
@@ -22,10 +26,10 @@ class Resource(object):
         self.res_type = res_type
         self.spreadsheet = {}
 
-        Resource.region_names = []  # dunno why "Resource" has to be there
+        self.region_names = []
         self.get_regions()
 
-        Resource.credentials = self.get_credentials()
+        self.credentials = self.get_credentials()
 
         # populate depending on type
         if self.res_type == "instance":
@@ -37,39 +41,34 @@ class Resource(object):
         elif self.res_type == "image":
             self.populate_images()
 
-    @staticmethod
-    def get_credentials():
+    def get_credentials(self):
         return {"aws_access_key_id": os.environ['AWS_ACCESS_KEY'],
                 "aws_secret_access_key": os.environ['AWS_SECRET_KEY']}
 
-    @staticmethod
-    def get_regions():
+    def get_regions(self):
         regions = ec2.regions()
         for region in regions:
-            Resource.region_names.append(region.name)
+            self.region_names.append(region.name)
 
-    @staticmethod
-    def get_volumes(region):
+    def get_volumes(self, region):
         """Return list of whole volumes for a given region"""
         try:
-            conn = ec2.connect_to_region(region, **Resource.credentials)
+            conn = ec2.connect_to_region(region, **self.credentials)
             region_volumes = conn.get_all_volumes()
         except boto.exception.EC2ResponseError:
             return []  # This better not fail silently or I'll cut a person.
         return region_volumes
 
-    @staticmethod
-    def get_all_volumes():
+    def get_all_volumes(self):
         all_volumes = []
-        for region in Resource.region_names:
-            all_volumes.extend(Resource.get_volumes(region))
+        for region in self.region_names:
+            all_volumes.extend(self.get_volumes(region))
         return all_volumes
 
-    @staticmethod
-    def get_instances(region):
+    def get_instances(self, region):
         """Return list of whole instances for given region"""
         try:
-            conn = ec2.connect_to_region(region, **Resource.credentials)
+            conn = ec2.connect_to_region(region, **self.credentials)
             region_instances = []
             reservations = conn.get_all_reservations()
             for reservation in reservations:
@@ -79,28 +78,25 @@ class Resource(object):
             return []
         return region_instances
 
-    @staticmethod
-    def get_all_instances():
+    def get_all_instances(self):
         all_instances = []
-        for region in Resource.region_names:
-            all_instances.extend(Resource.get_instances(region))
+        for region in self.region_names:
+            all_instances.extend(self.get_instances(region))
         return all_instances
 
-    @staticmethod
-    def get_snapshots(region):
+    def get_snapshots(self, region):
         """Return list of whole snapshots for a given region"""
         try:
-            conn = ec2.connect_to_region(region, **Resource.credentials)
+            conn = ec2.connect_to_region(region, **self.credentials)
             region_snapshots = conn.get_all_snapshots(owner='self')
         except boto.exception.EC2ResponseError:
             return []
         return region_snapshots
 
-    @staticmethod
-    def get_all_snapshots():
+    def get_all_snapshots(self):
         all_snapshots = []
-        for region in Resource.region_names:
-            all_snapshots.extend(Resource.get_snapshots(region))
+        for region in self.region_names:
+            all_snapshots.extend(self.get_snapshots(region))
         return all_snapshots
 
     @staticmethod
@@ -145,33 +141,31 @@ class Resource(object):
                 snapshot_ids.append(device_mapping[device].snapshot_id.encode())  # do I need to have 'encode' here?
         return snapshot_ids
 
-    @staticmethod
-    def get_images(region):
+    def get_images(self, region):
         """Get whole AMIs for a given region"""
         try:
-            conn = ec2.connect_to_region(region, **Resource.credentials)
+            conn = ec2.connect_to_region(region, **self.credentials)
             region_images = conn.get_all_images(owners=['self'])
         except boto.exception.EC2ResponseError:
             return []
         return region_images
 
-    @staticmethod
-    def get_all_images():
+    def get_all_images(self):
         all_images = []
-        for region in Resource.region_names:
-            all_images.extend(Resource.get_images(region))
+        for region in self.region_names:
+            all_images.extend(self.get_images(region))
         return all_images
 
     def populate_images(self):
         """Dict of dicts for images"""
         print "Populating images info..."
-        images = Resource.get_all_images()
+        images = self.get_all_images()
         for i in images:
 
-            associated_snapshots = Resource.get_snapshots_of(i)
+            associated_snapshots = self.get_snapshots_of(i)
 
-            self.spreadsheet[i.id] = dict(name=i.name, Name_tag=Resource.get_name_tag(i), id=i.id,
-                                          KEEP_tag=Resource.get_keep_tag(i), PROD_tag=Resource.is_production(i),
+            self.spreadsheet[i.id] = dict(name=i.name, Name_tag=self.get_name_tag(i), id=i.id,
+                                          KEEP_tag=self.get_keep_tag(i), PROD_tag=self.is_production(i),
                                           region=i.region.name,
                                           created=i.creationDate,
                                           associated_snapshots=associated_snapshots,
@@ -180,24 +174,20 @@ class Resource(object):
     def populate_volumes(self):
         """Dictionary of dictionaries representing volumes"""
         print "Populating volumes info..."
-        volumes = Resource.get_all_volumes()
+        volumes = self.get_all_volumes()
         for i in volumes:
 
             # handle associated instance's KEEP-tag
             associated_instance_id = i.attach_data.instance_id
 
-            # instance_keep_tag = (j for j in Ins.spreadsheet if j["id"] == associated_instance_id).next()['KEEP_tag']
-            # TODO: only kinda works but maybe better if I did a look-up on a dict of dicts, could timeit later.
-            # TODO: would be nice to figure out what the error means: StopIteration exception
-
             if associated_instance_id is None:  # sometimes there is no attached instance
                 instance_keep_tag = "-------no-instance-found"
             else:
                 instance_keep_tag = Ins.spreadsheet[associated_instance_id]['KEEP_tag']
-            self.spreadsheet[i.id] = dict(Name_tag=Resource.get_name_tag(i), id=i.id, KEEP_tag=Resource.get_keep_tag(i),
+            self.spreadsheet[i.id] = dict(Name_tag=self.get_name_tag(i), id=i.id, KEEP_tag=self.get_keep_tag(i),
                                           instance_KEEP_tag=instance_keep_tag,
                                           associated_instance_id=associated_instance_id,
-                                          PROD_tag=Resource.is_production(i), attachment_state=i.attachment_state(),
+                                          PROD_tag=self.is_production(i), attachment_state=i.attachment_state(),
                                           state=i.volume_state(), status=i.status, iops=i.iops, size=i.size,
                                           created=i.create_time, region=i.region.name)
 
@@ -206,27 +196,27 @@ class Resource(object):
         Dict is nice so that we can easily look up instance KEEP-tags later.
         """
         print "Populating instances info..."
-        instances = Resource.get_all_instances()
+        instances = self.get_all_instances()
         for i in instances:
-            self.spreadsheet[i.id] = dict(Name_tag=Resource.get_name_tag(i), id=i.id, KEEP_tag=Resource.get_keep_tag(i),
-                                          PROD_tag=Resource.is_production(i), instance_type=i.instance_type,
+            self.spreadsheet[i.id] = dict(Name_tag=self.get_name_tag(i), id=i.id, KEEP_tag=self.get_keep_tag(i),
+                                          PROD_tag=self.is_production(i), instance_type=i.instance_type,
                                           state=i.state, launched=i.launch_time, region=i.region.name)
 
     def populate_snapshots(self):
         """Dict of dicts for snapshots"""
         print "Populating snapshots info..."
-        snapshots = Resource.get_all_snapshots()
+        snapshots = self.get_all_snapshots()
 
         for i in snapshots:
 
             # find the ami id(s) for this snapshot. API allows for multiple even though I don't think there would be
-            associated_ami_ids = Resource.get_amis_of(i.id)
+            associated_ami_ids = self.get_amis_of(i.id)
 
             ami_keep_tags = [Ims.spreadsheet[ami_id]['KEEP_tag'] for ami_id in associated_ami_ids]
 
-            self.spreadsheet[i.id] = dict(Name_tag=Resource.get_name_tag(i), id=i.id, KEEP_tag=Resource.get_keep_tag(i),
+            self.spreadsheet[i.id] = dict(Name_tag=self.get_name_tag(i), id=i.id, KEEP_tag=self.get_keep_tag(i),
                                           ami_KEEP_tag=ami_keep_tags, associated_ami_ids=associated_ami_ids,
-                                          PROD_tag=Resource.is_production(i), start_time=i.start_time,
+                                          PROD_tag=self.is_production(i), start_time=i.start_time,
                                           region=i.region.name, associated_volume=i.volume_id,
                                           volume_size=i.volume_size, description=i.description)
 
@@ -331,9 +321,9 @@ def generate_images_report():
 
 def main():
     # import pdb; pdb.set_trace()
-    # generate_volumes_report()
-    # generate_snapshots_report()
-    # generate_instances_report()
+    generate_volumes_report()
+    generate_snapshots_report()
+    generate_instances_report()
     generate_images_report()
     print "done"
 
